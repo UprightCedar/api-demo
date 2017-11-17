c3c8691d382fe32025971afdc04fb151ccf05fe7
burning <burning0xb@163.com>
2017-11-17 18:50:59 +0800

    del

--- src/routers/BaseRouter.js
+++ src/routers/BaseRouter.js
@@ -1,12 +1,7 @@
-import requireLogin from '../../config/requireLogin.json';
-
 export default class BaseRouter {
 
   constructor() {
     this.whiteList = [];
-    requireLogin.map((key) => {
-      this.whiteList.push(`/api${key}`);
-    });
   }
 
 }
